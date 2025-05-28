0c099bed226409996fcdafd3335226c308363a5f
orcsik.roland@stud.uni-obuda.hu <orcsik.roland@stud.uni-obuda.hu>
2025-05-28 14:48:45 +0200

    feat: add imports for Observable, HttpClient, and WorkoutSession in WorkoutSessionService

--- workout-tracker/src/app/services/workout-session.service.ts
+++ workout-tracker/src/app/services/workout-session.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { WorkoutSession } from '../models/workout-session';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WorkoutSessionService {
 
-  constructor() { }
+  private apiUrl = 'https://localhost:5001/api/workoutsession';
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<WorkoutSession[]> {
+    return this.http.get<WorkoutSession[]>(this.apiUrl);
+  }
+  
 }
