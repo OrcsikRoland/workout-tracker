40d1a75fabd872a02ccfcf2d60c2d38a89c2455d
orcsik.roland@stud.uni-obuda.hu <orcsik.roland@stud.uni-obuda.hu>
2025-05-28 20:54:45 +0200

    feat: add WorkoutSessionForm component with routing and basic structure

--- workout-tracker/src/app/app-routing.module.ts
+++ workout-tracker/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { WorkoutSessionListComponent } from './pages/workout-session-list/workout-session-list.component';
 import { HomeComponent } from './pages/home/home.component';
+import { WorkoutSessionFormComponent } from './pages/workout-session-form/workout-session-form.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'sessions', component: WorkoutSessionListComponent },
+  { path: 'session/new', component: WorkoutSessionFormComponent},
 ];
 
 @NgModule({
