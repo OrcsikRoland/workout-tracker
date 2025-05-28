68824b05b9c21a60596690d9ce80e58cb7d8a3bc
orcsik.roland@stud.uni-obuda.hu <orcsik.roland@stud.uni-obuda.hu>
2025-05-28 14:41:38 +0200

    feat: implement WorkoutSession model and service

---
+++ workout-tracker/src/app/models/workout-session.spec.ts
@@ -0,0 +1,7 @@
+import { WorkoutSession } from './workout-session';
+
+describe('WorkoutSession', () => {
+  it('should create an instance', () => {
+    expect(new WorkoutSession()).toBeTruthy();
+  });
+});
---
+++ workout-tracker/src/app/models/workout-session.ts
@@ -0,0 +1,8 @@
+export class WorkoutSession {
+    id: number = 0;
+    date: string = '';
+    workoutTypeId: number = 0;
+    durationMinutes: number = 0;
+    caloriesBurned: number = 0;
+    notes?: string; 
+}
