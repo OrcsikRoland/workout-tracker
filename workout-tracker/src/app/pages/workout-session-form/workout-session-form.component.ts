41190cdaf8908acad8109c5dfdcc2d5c67b59e11
orcsik.roland@stud.uni-obuda.hu <orcsik.roland@stud.uni-obuda.hu>
2025-05-29 16:56:47 +0200

    feat: enhance WorkoutSessionForm to support editing existing sessions by retrieving session data based on route ID

--- workout-tracker/src/app/pages/workout-session-form/workout-session-form.component.ts
+++ workout-tracker/src/app/pages/workout-session-form/workout-session-form.component.ts
@@ -4,6 +4,7 @@ import { WorkoutSessionService } from '../../services/workout-session.service';
 import { Router } from '@angular/router';
 import { WorkoutTypeService } from '../../services/workout-type.service';
 import { WorkoutType } from '../../models/workout-type';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-workout-session-form',
@@ -12,15 +13,35 @@ import { WorkoutType } from '../../models/workout-type';
   styleUrl: './workout-session-form.component.scss'
 })
 export class WorkoutSessionFormComponent implements OnInit {
+
   sessions: WorkoutSession = new WorkoutSession()
   workoutTypes: WorkoutType[] = [];
-  constructor(private sessionService: WorkoutSessionService, private router: Router, private workoutTypeService: WorkoutTypeService){}
+
+  constructor(
+    private sessionService: WorkoutSessionService,
+    private router: Router,
+    private workoutTypeService: WorkoutTypeService,
+    private route: ActivatedRoute){}
+
+
   ngOnInit(): void {
     this.workoutTypes = this.workoutTypeService.getAll();
+    let idParam = this.route.snapshot.paramMap.get('id');
+    if (idParam) {
+      const id = +idParam;
+      const existing = this.sessionService.getById(id);
+      if (existing) {
+        this.sessions = existing;
+      }
+    }
   }
 
   save(): void {
-    this.sessionService.create(this.sessions);
+    if (this.sessions.id) {
+      this.sessionService.update(this.sessions.id, this.sessions);
+    } else {
+      this.sessionService.create(this.sessions);
+    }
     this.router.navigate(['/sessions']);
   }
 
