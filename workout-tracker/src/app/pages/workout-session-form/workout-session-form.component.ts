46850de43a3c35174dc246714818c7f5556851f8
orcsik.roland@stud.uni-obuda.hu <orcsik.roland@stud.uni-obuda.hu>
2025-05-29 14:33:47 +0200

    feat: enhance WorkoutSessionForm component with OnInit lifecycle and workout types retrieval

--- workout-tracker/src/app/pages/workout-session-form/workout-session-form.component.ts
+++ workout-tracker/src/app/pages/workout-session-form/workout-session-form.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { WorkoutSession } from '../../models/workout-session';
 import { WorkoutSessionService } from '../../services/workout-session.service';
 import { Router } from '@angular/router';
+import { WorkoutTypeService } from '../../services/workout-type.service';
+import { WorkoutType } from '../../models/workout-type';
+
 
 @Component({
   selector: 'app-workout-session-form',
@@ -9,10 +12,13 @@ import { Router } from '@angular/router';
   templateUrl: './workout-session-form.component.html',
   styleUrl: './workout-session-form.component.scss'
 })
-export class WorkoutSessionFormComponent {
+export class WorkoutSessionFormComponent implements OnInit {
   sessions: WorkoutSession = new WorkoutSession()
-
-  constructor(private sessionService: WorkoutSessionService, private router: Router){}
+  workoutTypes: WorkoutType[] = [];
+  constructor(private sessionService: WorkoutSessionService, private router: Router, private workoutTypeService: WorkoutTypeService){}
+  ngOnInit(): void {
+    this.workoutTypes = this.workoutTypeService.getAll();
+  }
 
   save(): void {
     this.sessionService.create(this.sessions);
