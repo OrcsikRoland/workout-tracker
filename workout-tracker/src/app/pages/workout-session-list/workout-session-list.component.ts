ba5efbd818e41fa8a609fa57df2050bb3d68ef56
orcsik.roland@stud.uni-obuda.hu <orcsik.roland@stud.uni-obuda.hu>
2025-05-30 14:05:27 +0200

    fix: correct sessionService reference and refactor session loading methods for improved clarity

--- workout-tracker/src/app/pages/workout-session-list/workout-session-list.component.ts
+++ workout-tracker/src/app/pages/workout-session-list/workout-session-list.component.ts
@@ -18,34 +18,52 @@ export class WorkoutSessionListComponent implements OnInit {
   workoutTypes: WorkoutType[] = [];
 
   constructor(
-    private sesssionService: WorkoutSessionService,
+    private sessionService: WorkoutSessionService,
     private workoutTypeService: WorkoutTypeService,
     private router: Router
   ) {}
   
   
   ngOnInit(): void {
-    this.sessions = this.sesssionService.getAll().sort((a, b) => b.id - a.id);
-    this.workoutTypes = this.workoutTypeService.getAll();
+    this.loadSessions();
+    this.loadWorkoutTypes();
+  }
+
+  loadSessions(): void {
+    this.sessionService.getAll().subscribe({
+      next: (data) => {
+        this.sessions = data.sort((a, b) => b.id - a.id);
+      },
+      error: (err) => console.error(err)
+    });
+  }
+  
+  loadWorkoutTypes(): void {
+    this.workoutTypeService.getAll().subscribe({
+      next: (data) => {
+        this.workoutTypes = data;
+      },
+      error: (err) => console.error(err)
+    });
   }
 
   getWorkoutTypeName(id: number): string {
-    let type = this.workoutTypes.find(t => t.id === id)
-    return type ? type.name :'ismeretlen' 
+    let type = this.workoutTypes.find(t => t.id === id);
+    return type ? type.name : 'Ismeretlen';
   }
+
 
   editSession(id: number): void {
     this.router.navigate(['/sessions/edit', id]);
   }
 
   deleteSession(id: number): void {
-    const confirmDelete = confirm('Are you sure you want to delete this workout session?')
-    if (confirmDelete) {
-      this.sesssionService.delete(id);
-      this.sessions = this.sesssionService.getAll(); 
+    if (confirm('Are you sure you want to delete this workout session?')) {
+      this.sessionService.delete(id).subscribe({
+        next: () => this.loadSessions(),
+        error: (err) => console.error(err)
+      });
     }
-
-
   }
 
 
